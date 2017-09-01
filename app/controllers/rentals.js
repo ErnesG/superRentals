4c5857baad0671ded1cf8f1fb6f337f8344a86d3
Ernesto Gon <egonzl92@outlook.com>
2017-08-31 21:49:08 -0500

    adds out-sync protection via promise and passing filter as argument

--- app/controllers/rentals.js
+++ app/controllers/rentals.js
@@ -4,10 +4,16 @@ export default Ember.Controller.extend({
     actions:{
         filterByCity(param){
             if(param!==''){
-                return this.get('store').query('rental',{city: param});
+                return this.get('store').query('rental',{city: param}).then(
+                    (results)=>{
+                        return {query: param, results:results};
+                    }
+                );
             }
             else{
-                return this.get('store').findAll('rental');
+                return this.get('store').findAll('rental').then((results)=>{
+                    return {query: param, results: results};
+                });
             }
         }
     }
