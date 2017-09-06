30cf62737fa3b664d1cd0188df8872dda659ea1a
Ernesto Gon <egonzl92@outlook.com>
2017-09-05 22:39:16 -0500

    lol

--- app/components/list-filter.js
+++ app/components/list-filter.js
@@ -6,14 +6,20 @@ export default Ember.Component.extend({
     init(){
         this._super(...arguments);
         this.get('filter')('').then(
-            (results) => this.set('results',results));
+            (allResults) => this.set('results',allResults.results));
     },
     actions: {
         handleFilterEntry(){
             let filterInputValue = this.get('value');
             let filterAction = this.get('filter');
             filterAction(filterInputValue).then(
-                (filterResults) => this.set('results', filterResults));
+                (resultsObj) =>{
+                    if(resultsObj.query ===  this.get('value')){
+                        this.set('results', resultsObj.filterResults);
+
+                    }
+                    
+            } );
         }
     }
 
